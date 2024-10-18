783ade302e0306f0b2c18790556e4484da16b9c5
DESKTOP-BBCK0OA\csi22 <david.rodriguez.alonso10@gmail.com>
2024-10-18 09:59:34 +0200

    first comimit

---
+++ main.js
@@ -0,0 +1,36 @@
+/* *********************************
+EJERCICIO 2. CREA EL CONTROLADOR DE LA PÁGINA. DEBERÁ PODER CAMBIAR LA COLUMNA DE LA IZQUIERDA PARA QUE MUESTRE LOS DATOS SOLICITADOS Y EN LA COLUMNA DE LA DERECHA LA IMAGEN QUE CORRESPONDE.
+EN CONCRETO, HAY QUE MOSTRAR UNA LISTA DE PRÉSTAMOS Y UNA LISTA DE SOCIOS
+************************************ */
+
+
+// EJERCICIO 2.1 Importar las funciones correspondientes
+import { listaPrestamos, imgPrestamos, listaSocios, imgSocios} from './funciones.js';
+
+
+// EJERCICIO 2.2 Crear las funciones de control de contenidos
+//      * Una función (o dos) que solicitará el listado formateado de préstamos/de socios y el nombre de la imagen correspondiente
+//      * Luego colocará el listado en el bloque de la izquierda sustituyendo lo que allí aparezca
+//      * Para terminar, colocará la imagen en la columna de la derecha
+function listadoPrestamos() {
+
+    const listadoPrestamos = listaPrestamos();
+    const prestamos = document.querySelector('#prestamos');
+    prestamos.listaPrestamos = '';
+    prestamos.append(listadoPrestamos);
+    document.append(imgPrestamos());
+
+}
+
+function listadoSocios() {
+    const listadoSocios = listaSocios();
+    const socios = document.querySelector('#socios');
+    socios.listaSocios = '';
+    socios.append(listadoSocios);
+    document.append(imgSocios());
+   
+}
+
+// EJERCICIO 2.3 Enlazar las funciones del módulo main con las funciones del objeto window
+window.listadoPrestamos=listadoPrestamos;
+window.listadoSocios=listadoSocios;
